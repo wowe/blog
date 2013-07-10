1ca9daed77fdb6c5f295df429f2cd8abc1db92c8
wowe <woebookpro@gmail.com>
2013-07-10 10:22:38 +0700

    method READ

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,34 @@
 class PostsController < ApplicationController
+    def show
+        @post = Post.find(params[:id])
+    end
+
+
     def new
     end
     
     
-    def create
-        render text: params[:post].inspect
+    def index
+        @posts = Post.all
     end
+
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+
+    def create
+        @post = Post.new(post_params)
+        if @post.save
+            redirect_to @post
+        else
+            render "new"
+        end
+#        render text: params[:post].inspect
+    end
+    private
+        def post_params
+            params.require(:post).permit(:title, :text)
+        end
 end
