94cbd1a0bbfcdb950c52ccf2849ef759fafa1feb
wowe <woebookpro@gmail.com>
2013-08-12 10:00:07 +0700

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = 'd7e9b63f42f8b7c00560e311b1b75464abfde68dbebcacd6412e59880c3ca05dd6b8ad82eb7bfddcdb83338558d1402eef197307451096b23659a997a7468cf5'
